f0d99eb96a69ccf6ca2bf2283c1cc2ffb0094305
lamlengend98 <lamnguyen98tb@gmail.com>
2019-01-21 09:39:13 +0700

    demo api

--- lamworkspace/nodejs/test.js
+++ lamworkspace/nodejs/test.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Test Javascript</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div id="text"></div>
+	<script>
+		var num1 = parseInt(prompt("Insert first number: ",""));
+		var num2 = parseInt(prompt("Insert second number",""));
+		var sum;
+		sum = (num1 + num2);
+		document.getElementById('text').innerHTML = sum;
+	</script>
+</body>
+</html>
