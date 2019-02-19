048004b2fd1931395c9286578540109e248aff9b
lamlengend98 <lamnguyen98tb@gmail.com>
2019-02-19 22:55:22 +0700

    demo crud spring boot

---
+++ lamworkspace/Spring Boot/MyEmpolyee/demo/src/main/java/com/example/demo/dao/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface EmployeeDAO extends JpaRepository<Employee, Integer> {
+    List<Employee> findByNameContaining(String q);
+
+    Page<Employee> findByIdNotNull(Pageable pageable);
+
+    List<Employee> findById(int id);
+}
