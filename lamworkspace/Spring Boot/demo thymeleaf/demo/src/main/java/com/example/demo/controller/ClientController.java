9822f5a2854bc2f9ea3e573f5a8be8673d36b9eb
lamlengend98 <37770203+lamlengend98@users.noreply.github.com>
2019-02-25 09:32:38 +0700

    Update ClientController.java

--- lamworkspace/Spring Boot/demo thymeleaf/demo/src/main/java/com/example/demo/controller/ClientController.java
+++ lamworkspace/Spring Boot/demo thymeleaf/demo/src/main/java/com/example/demo/controller/ClientController.java
@@ -1,16 +1,13 @@
 package com.example.demo.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 //import com.example.demo.dao.EmployeeDAO;
 import com.example.demo.dao.EmployeeDAO;
 import com.example.demo.model.Employee;
-import com.example.demo.model.PageModel;
+import com.example.demo.model.PagerModel;
 import com.example.demo.service.EmployeeService;
-import com.example.demo.service.PageWrapper;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,37 +21,38 @@ public class ClientController {
     private static final int INITIAL_PAGE_SIZE = 5;
     private static final int[] PAGE_SIZES = {5, 10};
 
-    @Autowired
-    EmployeeDAO employeeDAO;
-    @GetMapping("/")
-    public ModelAndView homepage(@RequestParam("pageSize") int pageSize,
-                                 @RequestParam("page") int page) {
-        page = page - 1;
-        List<Employee> relativeSentAtEmailList = new ArrayList<Employee>();
-        PageRequest pageRequest = new PageRequest(page, PAGE_SIZE, Sort.Direction.DESC, "sentAt");
-        Page<Employee> pages = search == null || search.length() == 0? mailBoxService.list(pageRequest) : mailBoxService.searchContent(search, pageRequest);
-        List<Employee> list = pages.getContent();
-        int rowsInPage = list.size();
-        PageWrapper<Employee> pageWrapper = new PageWrapper<Employee>(pages, "/admin/mailbox");
-        for(int i = 0; i < rowsInPage; i++){
-            Email email = list.get(i);
-            RelativeSentAtEmail relativeSentAtEmail = new RelativeSentAtEmail(email);
-            List<EmailAccount> listAccount = mailAccountsService.findById(email.getAccountId());
-            EmailAccount emailAccount = listAccount.size() > 0 ? listAccount.get(0) : null;
-            String emailAccountAddress = emailAccount != null ? emailAccount.getAccount() : "Unknown";
-            relativeSentAtEmail.setAccount(emailAccountAddress);
-            relativeSentAtEmailList.add(relativeSentAtEmail);
-        }
-        if(search != null && search.length() > 0){
-            model.addAttribute("search", search);
-        }
-        int fromEntry = rowsInPage == 0 ? 0 : page * PAGE_SIZE + 1;
-        int toEntry = rowsInPage == 0 ? 0 : fromEntry + rowsInPage - 1;
-        model.addAttribute("list", relativeSentAtEmailList);
-        model.addAttribute("page", pageWrapper);
-        model.addAttribute("fromEntry", fromEntry);
-        model.addAttribute("toEntry", toEntry);
-        return modelAndView;
+@Autowired
+EmployeeDAO employeeDAO;
+@GetMapping("/")
+public ModelAndView homepage(@RequestParam("pageSize") Optional<Integer> pageSize,
+                             @RequestParam("page") Optional<Integer> page) {
+    if (employeeDAO.count() != 0) {
+        ;//pass
+    } else {
+        employeeDAO.findAll();
     }
-
+    ModelAndView modelAndView = new ModelAndView("list");
+    //
+    // Evaluate page size. If requested parameter is null, return initial
+    // page size
+    int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+    // Evaluate page. If requested parameter is null or less than 0 (to
+    // prevent exception), return initial size. Otherwise, return value of
+    // param. decreased by 1.
+    int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+    // print repo
+    System.out.println("here is client repo " + employeeDAO.findAll());
+    Page<Employee> clientlist = (Page<Employee>) employeeDAO.findAll(new PageRequest(evalPage, evalPageSize));
+    System.out.println("client list get total pages" + clientlist.getTotalPages() + "client list get number " + clientlist.getNumber());
+    PagerModel pager = new PagerModel(clientlist.getTotalPages(), clientlist.getNumber(), BUTTONS_TO_SHOW);
+    // add clientmodel
+    modelAndView.addObject("clientlist", clientlist);
+    // evaluate page size
+    modelAndView.addObject("selectedPageSize", evalPageSize);
+    // add page sizes
+    modelAndView.addObject("pageSizes", PAGE_SIZES);
+    // add pager
+    modelAndView.addObject("pager", pager);
+    return modelAndView;
+}
 }
